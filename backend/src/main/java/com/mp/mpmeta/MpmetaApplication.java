1aa823b6cf9db6939e1d31bee057815b52558370
Elias <eliascalado@hotail.com>
2022-11-16 14:28:37 -0300

    Project created

---
+++ backend/src/main/java/com/mp/mpmeta/MpmetaApplication.java
@@ -0,0 +1,13 @@
+package com.mp.mpmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mp/mpmeta/MpmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mp.mpmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
